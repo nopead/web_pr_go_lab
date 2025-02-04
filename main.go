8be4637ac4dd60fb69b89e00c188cbdc9446523e
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-04 13:12:54 +0300

    connected api to search handler, added rendering response

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
+	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+var apikey *string
 
 type Source struct {
 	ID   interface{} `json:"id"`
@@ -53,17 +59,60 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	params := u.Query()
-	searchkey := params.Get("q")
+	searchKey := params.Get("q")
 	page := params.Get("page")
 	if page == "" {
 		page = "1"
 	}
 
-	fmt.Println("Search Query is: ", searchkey)
-	fmt.Println("Results page is: ", page)
+	search := &Search{}
+	search.SearchKey = searchKey
+
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		return
+	}
+
+	search.NextPage = next
+	pageSize := 20
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apikey)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	err = tpl.Execute(w, search)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func main() {
+
+	apikey = flag.String("apikey", "", "Newsapi.org access key")
+	flag.Parse()
+
+	if *apikey == "" {
+		log.Fatal("apikey must be set")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
